d467c850057965ddd73f91084c9b05989d72caa8
stayz7p <97446172+stayz7p@users.noreply.github.com>
2022-01-25 00:30:01 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+let stars = document.getElementById('stars');
+let moon = document.getElementById('moon');
+let mountains3 = document.getElementById('mountains3');
+let mountains4 = document.getElementById('mountains4');
+let river = document.getElementById('river');
+let boat = document.getElementById('boat');
+let staynope = document.querySelector('.staynope');
+window.onscroll = function(){
+    let value = scrollX;
+    stars.style.left = value + 'px';
+    moon.style.top = value * 3 + 'px';
+    mountains3.style.top = value * 2 + 'px';
+    mountains4.style.top = value * 1.5 + 'px';
+    river.style.top = value + 'px';
+    boat.style.top = value * 1.5 + 'px';
+    boat.style.left = value * 3 + 'px';
+
+
+}
